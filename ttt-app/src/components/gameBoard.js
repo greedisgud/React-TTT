bf7d0364eea3dfa448eca0c796d8d36144c5a6e4
Evan Hayes <hayesevan7@gmail.com>
2022-02-22 19:55:33 -0500

    completed game logic

--- ttt-app/src/components/gameBoard.js
+++ ttt-app/src/components/gameBoard.js
@@ -1,30 +1,90 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Gameboard = () => {
-  const Cell = () => {
-    return <td>-</td>;
+  const [turn, setTurn] = useState("x");
+  const [cells, setCells] = useState(Array(9).fill(""));
+  const [winner, setWinner] = useState();
+
+  const checkWinner = (squares) => {
+    let combos = {
+      across: [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+      ],
+      down: [
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+      ],
+      diag: [
+        [0, 4, 8],
+        [2, 4, 6],
+      ],
+    };
+
+    for (let combo in combos) {
+      combos[combo].forEach((pattern) => {
+        if (
+          squares[pattern[0]] === "" ||
+          squares[pattern[1]] === "" ||
+          squares[pattern[2]] === ""
+        ) {
+          //nothing
+        } else if (
+          squares[pattern[0]] === squares[pattern[1]] &&
+          squares[pattern[1]] === squares[pattern[2]]
+        ) {
+          setWinner(squares[pattern[0]]);
+        }
+      });
+    }
   };
+
+  const handleClick = (num) => {
+    if (cells[num] !== "") {
+      alert("Taken");
+      return;
+    }
+    let squares = [...cells];
+    if (turn === "x") {
+      squares[num] = "x";
+      setTurn("o");
+    } else {
+      squares[num] = "o";
+      setTurn("x");
+    }
+    checkWinner(squares);
+    setCells(squares);
+  };
+
+  const Cell = ({ num }) => {
+    return <td onClick={() => handleClick(num)}>{cells[num]}</td>;
+  };
+
   return (
-    <div>
+    <div className="container">
+      Turn: {turn}
       <table>
         <tbody>
           <tr>
-            <Cell />
-            <Cell />
-            <Cell />
+            <Cell num={0} />
+            <Cell num={1} />
+            <Cell num={2} />
           </tr>
           <tr>
-            <Cell />
-            <Cell />
-            <Cell />
+            <Cell num={3} />
+            <Cell num={4} />
+            <Cell num={5} />
           </tr>
           <tr>
-            <Cell />
-            <Cell />
-            <Cell />
+            <Cell num={6} />
+            <Cell num={7} />
+            <Cell num={8} />
           </tr>
         </tbody>
       </table>
+      {winner && <p> {winner} is the winner!</p>}
     </div>
   );
 };
